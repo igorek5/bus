86a37479d54099bff86aada732dcb4711b4277de
igorek5 <igorbautin101@gmail.com>
2020-02-10 00:42:41 +0300

    Настроил сообщение Telegram

--- submit.php
+++ submit.php
@@ -1,14 +1,31 @@
 <?php
 if (!empty($_POST['name']) AND !empty($_POST['phone']) ){
   
-  $theme = 'Новое сообщение';
+$name = $_POST['name'];
+$phone = $_POST['phone']; 
+$message = $_POST['message']; 
+
+// $zakaz = "Заказ с сайты XXX";
+
+$token = "910847660:AAGcqPzqs9R1GtPR9MWMnEYLfBtJVvJ_uSE"; // Это ТОКЕН
+$chat_id = "918924559"; // Это ИД группы
+
+$arr = array(
+
+// 'Заказ с сайты: ' => $zakaz,
   
-  $letter = 'Данные сообщения:\r\n';
-  $letter .= 'Имя: '.$_POST['name'].'\r\n';
-  $letter .= 'Телефон: '.$_POST['phone'].'\r\n';
-  $letter .= 'Сообщение: '.$_POST['message'].'\r\n';
+  'Имя: ' => $name,
+  'Телефон: ' => $phone,
+  'Сообщение: ' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
   
- if (mail('igorbautin101@gmail.com', $theme, $letter)){
+  if ($sendToTelegram){
    echo "Сообщение отправлено!";
 } else {
   echo "Ошибка при отправке!";
